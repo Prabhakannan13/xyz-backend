e26fc1e0cbfcd098d661b67c41fc45e86835d91a
Lakshmi <Lakshmi@NTPLAP-008.chn.nestgroup.net>
2022-10-21 00:03:20 +0530

    push1

--- src/main/java/com/nestdigital/xyzbackend/Controller/StudentController.java
+++ src/main/java/com/nestdigital/xyzbackend/Controller/StudentController.java
@@ -3,10 +3,9 @@ package com.nestdigital.xyzbackend.Controller;
 import com.nestdigital.xyzbackend.Dao.StudentDao;
 import com.nestdigital.xyzbackend.Model.StudentModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -20,5 +19,11 @@ public class StudentController {
         System.out.println(student.toString());
         dao.save(student);
         return "{status:'success'}";
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/viewstudent")
+    public List<StudentModel>viewstudent(){
+        return (List<StudentModel>) dao.findAll();
     }
 }
